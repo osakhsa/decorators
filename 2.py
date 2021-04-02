5c70bbfdcccfa0d131052a524efc32ca171f7e43
osakhsa <osakhsa3@gmail.com>
2021-04-03 00:38:01 +0300

    Second task, fixed set of arguments

---
+++ 2.py
@@ -0,0 +1,24 @@
+import time
+
+
+def time_checker(func):
+
+    def wrapper(number):
+        t0 = time.time()
+        func(number)
+        t1 = time.time()
+        print("Time elapsed:", t1-t0, "seconds")
+    return wrapper
+
+
+@time_checker
+def useful_function(number=10101):
+    counter = 0
+    for i in range(number+1):
+        for k in range(i+1, number+1):
+            counter += i * k
+    counter = 1000000000
+    print("Very useful function finished its very useful work! Bravo!")
+
+
+useful_function(number=int(input()))
