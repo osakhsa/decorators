90ac634e3020963bd0c6e2c7f35dd439e70b5666
osakhsa <osakhsa3@gmail.com>
2021-04-03 11:24:29 +0300

    Solution without function modifying

--- 2.py
+++ 2.py
@@ -5,15 +5,15 @@ def time_checker(func):
 
     def wrapper(**kwargs):
         t0 = time.time()
-        func(kwargs)
+        func(**kwargs)
         t1 = time.time()
         print("Time elapsed:", t1-t0, "seconds")
     return wrapper
 
 
 @time_checker
-def useful_function(kwargs):
-    number = kwargs['number']
+def useful_function(number):
+    # number = kwargs['number']
     counter = 0
     for i in range(number+1):
         for k in range(i+1, number+1):
